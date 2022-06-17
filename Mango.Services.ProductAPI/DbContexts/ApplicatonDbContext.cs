efd517001cf7201f4c0841536e354d68d1041c9d
WW <WW@MIZAN>
2022-06-17 08:51:57 +0600

    Add project files.

---
+++ Mango.Services.ProductAPI/DbContexts/ApplicatonDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Mango.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductAPI.DbContexts
+{
+    public class ApplicatonDbContext : DbContext
+    {
+        public ApplicatonDbContext(DbContextOptions<ApplicatonDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product>   Products { get; set; }
+
+    }
+}
