880f324fa5d6b355b9725e4ce3ac44717d61f167
Md. Mizanur Rahman <mizan_ra@hotmail.com>
2022-06-18 20:04:09 +0600

    AddProductRepository

---
+++ Mango.Services.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Mango.Services.ProductAPI.Models.Dtos;
+
+namespace Mango.Services.ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductDto>> GetProduct();
+        Task<ProductDto> GetProductById(int productId);
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+        Task<bool> DeleteProduct(int productId);
+
+    }
+}
